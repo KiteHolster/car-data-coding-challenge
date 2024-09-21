a2fe64f07886088fd7a272d659bf81a2c6b627cd
Hyun Park <hwpar5@student.monash.edu>
2024-09-21 18:50:54 +1000

    removing some imports

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import anime from 'animejs';
 import Image from "next/image";
 import Title from "antd/es/typography/Title";
